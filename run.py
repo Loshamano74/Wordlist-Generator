aff182d541955270b7d6483f66824cf2a74f391e
Loshamano74 <137676635+Loshamano74@users.noreply.github.com>
2023-12-17 15:09:26 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
+import os
 import itertools
 
-
 ban = '''
-
                                                     '''
 
 print('''\033[91m
@@ -23,31 +22,52 @@ final = int(scale.split(':')[1])
 
 use_nouse = str(input("\n\033[36m[?] Do you want to enter personal data ? [y/N]: "))
 if use_nouse == 'y':
-	first_name = str(input("\n\033[36m[*] Fist Name: "))
-	last_name = str(input("\n\033[36m[*] Last Name: "))
-	birthday = str(input("\n\033[36m[*] Birthday: "))
-	month = str(input("\n\033[36m[*] Month: "))
-	year = str(input("\n\033[36m[*] Year: "))
-	chrs = first_name + last_name + birthday + month + year
+    first_name = str(input("\n\033[36m[*] First Name: "))
+    last_name = str(input("\n\033[36m[*] Last Name: "))
+    birthday = str(input("\n\033[36m[*] Birthday: "))
+    month = str(input("\n\033[36m[*] Month: "))
+    year = str(input("\n\033[36m[*] Year: "))
+    chrs = first_name + last_name + birthday + month + year
 else:
-	chrs = 'abcdefghijklmnopqrstuvwxyz'
-	pass
+    chrs = 'abcdefghijklmnopqrstuvwxyz'
+    pass
 
 chrs_up = chrs.upper()
 chrs_specials = '!\][/?.,~-=";:><@#$%&*()_+\' '
 chrs_numerics = '1234567890'
 
-file_name = input('\n\033[36m[!] Insert a name for your wordlist file: ')
-arq = open(file_name, 'w')
-if input('\n\033[36m[?] Do you want to use uppercase characters? (y/n): ') == 'y':
-	chrs = ''.join([chrs, chrs_up])
-if input('\n\033[36m[?] Do you want to use special characters? (y/n): ') == 'y':
-	chrs = ''.join([chrs, chrs_specials])
-if input('\n\033[36m[?] Do you want to use numeric characters? (y/n): ') == 'y':
-	chrs = ''.join([chrs, chrs_numerics])
+total_combinations = sum(len(chrs) ** i for i in range(start, final + 1))
+generated_combinations = 0
 
-for i in range(start, final+1):
-	for j in itertools.product(chrs, repeat=i):
-		temp = ''.join(j)
-		arq.write(temp + '\n')
+directory = input('\n\033[36m[!] Specify directory path or leave blank for current directory: ')
+if directory:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    os.chdir(directory)
+
+file_name = input('\n\033[36m[!] Insert a name for your wordlist file: ')
+file_name = os.path.join(directory, file_name + '.txt')
+
+arq = open(file_name, 'w')
+
+if input('\n\033[36m[?] Do you want to use uppercase characters? (y/n): ') == 'y':
+    chrs = ''.join([chrs, chrs_up])
+if input('\n\033[36m[?] Do you want to use special characters? (y/n): ') == 'y':
+    chrs = ''.join([chrs, chrs_specials])
+if input('\n\033[36m[?] Do you want to use numeric characters? (y/n): ') == 'y':
+    chrs = ''.join([chrs, chrs_numerics])
+
+for i in range(start, final + 1):
+    for j in itertools.product(chrs, repeat=i):
+        temp = ''.join(j)
+        arq.write(temp + '\n')
+        generated_combinations += 1
+
+        # Calculate and display the percentage
+        percentage = (generated_combinations / total_combinations) * 100
+        print(f"Completion: {percentage:.2f}%\r", end='', flush=True)
+
+# Ensure the completion reaches 100%
+print("Completion: 100.00%")
+
 arq.close()
